2e31cdbb658a00ccbc43044af61d50fde71a093c
ibran1 <ibranr@letsupgrade.in>
2024-09-13 16:09:12 +0530

    fix: minr changes fix

--- public/cta.js
+++ public/cta.js
@@ -5,23 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
                     <h1 class="font-Bebas-Neue text-white text-3xl md:text-[40px] mb-6">TAKE THE NEXT STEP</h1>
                     <div class="flex flex-col md:flex-row justify-center gap-4 px-4">
                         <button
-                            class="bg-orange-400 text-white  py-2 px-6 rounded focus:outline-none focus:shadow-outline"
-                            type="button">
+                            class="bg-orange-400 text-white py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+                            type="button" onclick="openModal()">
                             Enquire Now
                         </button>
-                        <button
-                            class="bg-orange-400 text-white  py-2 px-6 rounded focus:outline-none focus:shadow-outline"
-                            type="button">
+                        
+                        <!-- Changed the button to an anchor tag with target="_blank" -->
+                        <a href="https://calendly.com/calendy-itm/mba" target="_blank"
+                            class="bg-orange-400 text-white py-2 px-6 rounded focus:outline-none focus:shadow-outline text-center">
                             Schedule Callback
-                        </button>
+                        </a>
+
                         <button
-                            class="bg-orange-400 text-white  py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+                            class="bg-orange-400 text-white py-2 px-6 rounded focus:outline-none focus:shadow-outline"
                             type="button">
                             Apply
                         </button>
                     </div>
                 </div>
             </section>
+
     `;
 
   const ctaContainer = document.getElementById("cta-container");
